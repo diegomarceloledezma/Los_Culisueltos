34912c9409eec33acb6a501595c618e2c73e1895
LushoCallapa <luiscallapa2004@gmail.com>
2023-10-23 18:06:54 -0400

    Log In - Sign Up + Botones

--- app/src/main/java/com/progra/losculisueltos/LogIn.kt
+++ app/src/main/java/com/progra/losculisueltos/LogIn.kt
@@ -3,13 +3,9 @@ package com.progra.losculisueltos
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-
+class LogIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_log_in)
     }
-
 }
