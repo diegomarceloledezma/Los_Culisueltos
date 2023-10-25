c62d6654c03435dd0af9f096df3d8be7c3cbb1a7
ASUS ROG STRIX G17 <diegomarceloledezma@gmail.com>
2023-10-25 16:35:19 -0400

    JSON agregado, log in y sign up retoques

--- app/src/main/java/com/progra/losculisueltos/SignUpActivity.kt
+++ app/src/main/java/com/progra/losculisueltos/SignUpActivity.kt
@@ -3,18 +3,37 @@ package com.progra.losculisueltos
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.progra.losculisueltos.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignUpBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = Firebase.auth
         binding.crearCuenta.setOnClickListener {
-            val intent = Intent(this, LogInActivity::class.java)
-            startActivity(intent)
+            clickCrearCuenta(binding.correoEdit.text.toString(),binding.contraEdit.text.toString())
         }
     }
+
+    fun clickCrearCuenta(email: String, contrasena: String){
+        auth.createUserWithEmailAndPassword(email, contrasena)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val user = auth.currentUser
+                    val intent = Intent(this, LogInActivity::class.java)
+                    startActivity(intent)
+                } else {
+
+                }
+            }
+    }
+
 }
