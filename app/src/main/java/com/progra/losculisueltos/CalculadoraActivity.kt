8ed3cdd2c68fe068c9e06cec233db1fca97dc241
LushoCallapa <luiscallapa2004@gmail.com>
2023-10-27 17:23:17 -0400

    Calculadora Resultado Modificado

--- app/src/main/java/com/progra/losculisueltos/CalculadoraActivity.kt
+++ app/src/main/java/com/progra/losculisueltos/CalculadoraActivity.kt
@@ -2,21 +2,37 @@ package com.progra.losculisueltos
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.ArrayAdapter;
 import android.os.Bundle
 import android.widget.Toast
 import android.content.Context
+import android.widget.AdapterView
+import android.widget.AutoCompleteTextView
 import androidx.core.content.ContextCompat
 import com.progra.losculisueltos.CalculadoraResultadoActivity.Companion.CLAVE_INT
 import com.progra.losculisueltos.databinding.ActivityCalculadoraBinding
 class CalculadoraActivity : AppCompatActivity() {
     lateinit var binding: ActivityCalculadoraBinding
     val context: Context = this
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCalculadoraBinding.inflate(layoutInflater)
         setContentView(binding.root)
         var genero = 0
+        var opcion = -1
+        val mul = listOf(1.2,1.375,1.55,1.725,1.9)
+        val item = listOf("Sedentario","Poco Activo", "Activo Moderado", "Activo","Muy Activo")
+        val autoComplete: AutoCompleteTextView = binding.actividadFisica
+
+        val adapter = ArrayAdapter(this, R.layout.dropdown_item,item)
+
+        autoComplete.setAdapter(adapter)
+        autoComplete.onItemClickListener = AdapterView.OnItemClickListener{
+                adapterView, view, i, l ->
+            val itemSelected = adapterView.getItemAtPosition(i)
+            binding.opciones.hint = itemSelected.toString()
+            opcion=i
+        }
         binding.masculino1.setOnClickListener {
             genero = 1
             binding.masculino1.backgroundTintList = ContextCompat.getColorStateList(this, R.color.cyan)
@@ -32,18 +48,19 @@ class CalculadoraActivity : AppCompatActivity() {
             val edadText = binding.edadCal.text.toString()
             val pesoText = binding.pesoCal.text.toString()
             val estaturaText = binding.estaturaCal.text.toString()
-            if (genero!=0 && edadText.isNotEmpty() && pesoText.isNotEmpty() && estaturaText.isNotEmpty()) {
+            if (genero!=0 && edadText.isNotEmpty() && pesoText.isNotEmpty() && estaturaText.isNotEmpty() && opcion!=-1) {
                 val edad: Int = edadText.toInt()
                 val peso: Int = edadText.toInt()
                 val estatura: Int = estaturaText.toInt()
-                val tmb: Double
+                var tmb: Double
+
                 if(genero==1){
-                    tmb =  66 + (13.7*peso) + (5*estatura) - (6.8 * edad)
+                    tmb =  66.5 + (13.75 * peso) + (5.003*estatura) - (6.75 *edad)
                 }
                 else{
-                    tmb = 655 + (9.6 * peso) + (1.8*estatura) - (4.7* edad)
+                    tmb = 655.1 + (9.563 * peso) + (1.850*estatura) - (4.676*edad)
                 }
-
+                tmb = tmb * mul[opcion]
                 val numeroInt: Int = tmb.toInt()
                 val intent: Intent = Intent(context, CalculadoraResultadoActivity::class.java)
                 intent.putExtra(CLAVE_INT,numeroInt)
--- app/src/main/java/com/progra/losculisueltos/ComidaActivity.kt
+++ app/src/main/java/com/progra/losculisueltos/ComidaActivity.kt
@@ -1,11 +1,22 @@
 package com.progra.losculisueltos
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.progra.losculisueltos.databinding.ActivityComidaBinding
 
 class ComidaActivity : AppCompatActivity() {
+    lateinit var binding: ActivityComidaBinding
+    val context: Context = this
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_comida)
+        binding = ActivityComidaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textoMenuCalculadoraCalorica.setOnClickListener {
+            val intent: Intent = Intent(context, CalculadoraActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
