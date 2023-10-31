c7491b0789ff3f8f5abd9d3d41d9a30823c9fef5
ASUS ROG STRIX G17 <diegomarceloledezma@gmail.com>
2023-10-31 19:12:16 -0400

    fail navegation view y puse algunas imagenes mas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation("com.github.bumptech.glide:glide:4.13.0")
     implementation("androidx.fragment:fragment-ktx:1.6.1")
+    implementation("com.google.code.gson:gson:2.8.8")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
